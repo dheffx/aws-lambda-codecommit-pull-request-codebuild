5a51d344a418cb0be719d1b4421ce22e7390c580
Daniel Heffner <dheff84@gmail.com>
2018-03-14 00:48:13 -0400

    add eslint and make adjustments based out results of eslint

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 'use strict'
+
 const buildEvent = require('./lib/build-event')
 const pullEvent = require('./lib/pull-event')
+
 /**
   Detect which event type is received and route it to the correct handler
 */
-exports.handler = (event, context) => {
+exports.handler = (event) => {
   try {
-    if (event.source === "aws.codebuild" && event['detail-type'] == "CodeBuild Build State Change") {
+    if (event.source === "aws.codebuild" && event['detail-type'] === "CodeBuild Build State Change") {
       buildEvent.handle(event.detail)
-    } else if (event.source === "aws.codecommit" && event['detail-type'] == "CodeCommit Pull Request State Change") {
+    } else if (event.source === "aws.codecommit" && event['detail-type'] === "CodeCommit Pull Request State Change") {
       pullEvent.handle(event.detail)
     } else {
       console.error("unspecified event received", event.source, event["detail-type"])
     }
-  } catch(e) {
+  } catch (e) {
     console.error(e)
   }
 }
